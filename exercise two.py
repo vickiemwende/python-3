5eafcbf8b9906166ba50acd74b407394b7323943
eMobilis <mwendevickie02@gmail.com>
2023-02-21 16:26:51 +0300

    my first commit

---
+++ exercise two.py
@@ -0,0 +1,2 @@
+num1=int(input("Enter your age in years"))
+print("you have lived" ,num1*12,"months")
---
+++ new variables.py
@@ -0,0 +1,11 @@
+
+name="vicky"
+print(name)
+age="20 years"
+print(age)
+gender="female"
+print(gender)
+hobby="reading stories"
+print(hobby)
+numbers="1,2,3,4,5,6,7,8,9,10"
+print(numbers)
